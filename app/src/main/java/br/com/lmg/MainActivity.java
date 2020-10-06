cf8c69b110a17ecf8287cb47b93305a4f032fc20
Lucas Maciel <lucasmacielgois@gmail.com>
2020-10-06 19:47:41 -0300

    'Commit'

--- app/src/main/java/br/com/lmg/MainActivity.java
+++ app/src/main/java/br/com/lmg/MainActivity.java
@@ -2,30 +2,30 @@ package br.com.lmg;
 
         import android.app.Activity;
         import android.os.Bundle;
-        import android.view.View;
         import android.widget.ArrayAdapter;
         import android.widget.ListView;
         import android.widget.TextView;
-        import android.widget.Toast;
 
         import java.util.ArrayList;
         import java.util.Arrays;
         import java.util.List;
 
         import androidx.annotation.Nullable;
+        import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         TextView aluno = new TextView(this);
         setContentView(R.layout.activity_main);
+        setTitle("Anotações");
 
-        List<String> alunos = new ArrayList<>(Arrays.asList("Alex", "Fran", "José", "Lucas", "Maria", "Pedro"));
+        List<String> alunos = new ArrayList<String>(Arrays.asList("Alex", "Fran", "José", "Lucas", "Maria", "Pedro"));
 
-        ListView listaDeAlunos = findViewById(R.id.activity_main_lista_de_alunos);
-        listaDeAlunos.setAdapter(new ArrayAdapter<>(
+        ListView listaDeAlunos = (ListView) findViewById(R.id.activity_main_lista_de_alunos);
+        listaDeAlunos.setAdapter(new ArrayAdapter<String>(
                 this,
                  android.R.layout.simple_list_item_1,
                  alunos));
