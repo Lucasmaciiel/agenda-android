cb863157a914fa1d5550243d850574cfe91f7680
Lucas Maciel <lucasmacielgois@gmail.com>
2020-10-06 21:01:50 -0300

    Create activity register

---
+++ app/src/main/java/br/com/lmg/ui/activity/CadastroActivity.java
@@ -0,0 +1,15 @@
+package br.com.lmg.ui.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import br.com.lmg.R;
+
+import android.os.Bundle;
+
+public class CadastroActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastro);
+    }
+}
---
+++ app/src/main/java/br/com/lmg/ui/activity/ListaAnotacoesActivity.java
@@ -0,0 +1,34 @@
+package br.com.lmg.ui.activity;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import br.com.lmg.R;
+
+public class ListaAnotacoesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        TextView aluno = new TextView(this);
+        setContentView(R.layout.activity_lista_anotacoes);
+        setTitle("Anotações");
+
+        List<String> alunos = new ArrayList<String>(Arrays.asList("Alex", "Fran", "José", "Lucas", "Maria", "Pedro"));
+
+        ListView listaDeAlunos = (ListView) findViewById(R.id.activity_lista_anotacoes_listView);
+        listaDeAlunos.setAdapter(new ArrayAdapter<String>(
+                this,
+                 android.R.layout.simple_list_item_1,
+                 alunos));
+
+    }
+}
